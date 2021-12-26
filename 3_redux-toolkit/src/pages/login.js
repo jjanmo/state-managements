669f5148f11ab24a9bd0a10ebf2e58422c62c2b0
jjanmo <jjanmo07@hanmail.net>
2021-12-27 01:25:50 +0900

    feat: add login/join page just finished styling

--- 3_redux-toolkit/src/pages/login.js
+++ 3_redux-toolkit/src/pages/login.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/login.module.css';
+import styles from '../styles/uForm.module.css';
 
 const Login = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>로그인</h1>
       <form className={styles.form}>
-        <input type="text" placeholder="이메일 입력" />
-        <input type="password" placeholder="비밀번호 입력" />
+        <input className={styles.input} type="text" placeholder="이메일 입력" />
+        <input className={styles.input} type="password" placeholder="비밀번호 입력" />
         <button className={styles.button}>로그인</button>
       </form>
       or
