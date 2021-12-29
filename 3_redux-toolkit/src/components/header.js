a4fc34e9ac112b6ab04315202d645b1156dbca60
jjanmo <jjanmo07@hanmail.net>
2021-12-30 05:05:46 +0900

    feat: display authenticated user's post count

--- 3_redux-toolkit/src/components/header.js
+++ 3_redux-toolkit/src/components/header.js
@@ -1,32 +1,38 @@
-import React, { useCallback, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as authActions from '../modules/actions/auth';
 import styles from '../styles/header.module.css';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.data);
+  const user = useSelector((state) => state.auth.user);
+  const [count, setCount] = useState(0);
 
   const onClickLogout = useCallback(() => {
     dispatch(authActions.logout());
   }, []);
+
+  useEffect(() => {
+    const name = user.nickname;
+    const _count = posts.filter((post) => post.author === name).length;
+    setCount(_count);
+  }, [posts]);
 
   return (
     <div className={styles.lContainer}>
       <div className={styles.searchContainer}>
         <div className={styles.result}>
           <span className={styles.name}>JJanmo</span>
-          님의 포스트 : <button className={styles.count}>200</button>개
+          님의 포스트 : <span className={styles.count}>{count}</span>개
         </div>
-        {/* <input className={styles.input} type="text" placeholder="포스트 검색" /> */}
         <Link className={styles.link} to="/form">
           포스트 추가
         </Link>
       </div>
       <div className={styles.userInfoContainer}>
-        <div>
-          <button>JJanmo</button>님
-        </div>
+        <div>JJanmo</div>
         <div>jjanmo@hanmail.net</div>
         <button className={styles.button} onClick={onClickLogout}>
           로그아웃
