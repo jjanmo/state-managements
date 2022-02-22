283d9015c738e9191e81ecbc5998c6a06916da73
jjanmo <jjanmo07@hanmail.net>
2022-02-23 00:44:27 +0900

    feat: setup tailwindcss in react project #2

--- 7_recoil/src/App.tsx
+++ 7_recoil/src/App.tsx
@@ -8,13 +8,21 @@ function App() {
   const onClickMinus = () => setCount(count - 1);
 
   return (
-    <div className="bg-gray-200 flex items-center justify-center h-screen">
-      <h1>{count}</h1>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <h1 className="text-blue-500 font-bold text-2xl">{count}</h1>
       <div>
-        <button className="text-red-600" onClick={onClickPlus}>
+        <button
+          className="text-3xl bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          onClick={onClickPlus}
+        >
           +
         </button>
-        <button onClick={onClickMinus}>-</button>
+        <button
+          className="text-3xl  px-8 py-3 border border-transparent font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+          onClick={onClickMinus}
+        >
+          -
+        </button>
       </div>
     </div>
   );
--- 7_recoil/src/index.tsx
+++ 7_recoil/src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
-import './tailwind.output.css';
+import GlobalStyles from './GlobalStyles';
+import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
+      <GlobalStyles />
       <App />
     </RecoilRoot>
   </React.StrictMode>,
