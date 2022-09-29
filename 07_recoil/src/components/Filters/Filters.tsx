c12710e95c9220ec9a67b038762bfd5f38a61c43
jjanmo <jjanmo07@hanmail.net>
2022-09-29 23:46:09 +0900

    feat: add filter active todos

--- 07_recoil/src/components/Filters/Filters.tsx
+++ 07_recoil/src/components/Filters/Filters.tsx
@@ -1,9 +1,13 @@
 import * as S from './Filters.style'
 import PriorityCheckbox from './Filters.priority'
+import { useRecoilValue } from 'recoil'
+import { todoListAtom } from '../../recoil/todo/atom'
 
 const priority = ['high', 'middle', 'low']
 
 function Filter() {
+  const todoList = useRecoilValue(todoListAtom)
+
   return (
     <S.Container>
       <S.Column>
@@ -14,7 +18,7 @@ function Filter() {
       <S.Column>
         <S.FilterTitle>Remaining Todos</S.FilterTitle>
         <S.LeftText>
-          <em>1</em> item left
+          <em>{todoList.filter((todo) => todo.status === 'active').length}</em> item left
         </S.LeftText>
       </S.Column>
       <S.Column>
