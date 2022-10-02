46dc323ea9244240d24ec176eeb09b74eddc8ebb
jjanmo <jjanmo07@hanmail.net>
2022-10-03 00:28:05 +0900

    feat: add connect redux to component

--- 4_redux-saga/src/components/counter/Counter.tsx
+++ 4_redux-saga/src/components/counter/Counter.tsx
@@ -1,14 +1,29 @@
+import { useAppDispatch, useAppSelector } from '../../store/useStore'
+import { syncIncrement, syncDecrement } from '../../store/syncCounter/syncCounterSlice'
+
 function Counter() {
+  const dispatch = useAppDispatch()
+  const syncCount = useAppSelector((state) => state.syncCounter.count)
+
+  console.log(syncIncrement, syncDecrement)
+  const handleSyncIncrement = () => {
+    dispatch(syncIncrement())
+  }
+  const handleSyncDecrement = () => {
+    dispatch(syncDecrement())
+  }
+
   return (
     <>
       <div>
         <h1>Sync Counter</h1>
-        <h2>1</h2>
+        <h2>{syncCount}</h2>
         <p>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={handleSyncIncrement}>+</button>
+          <button onClick={handleSyncDecrement}>-</button>
         </p>
       </div>
+      <hr />
       <div>
         <h1>Async Counter</h1>
         <h2>1</h2>
