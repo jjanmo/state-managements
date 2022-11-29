a720e98efb3d8c5648787a83912842a2b64f2dee
jjanmo <jjanmo07@hanmail.net>
2022-11-29 23:40:06 +0900

    feat: define filter-type for selector

--- 7_recoil/src/recoil/filter/atom.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil'
-import { FilterStatus } from './types'
-
-export const todoFilterStatusAtom = atom<FilterStatus>({
-  key: 'todoFilterStatus',
-  default: 'all',
-})
--- 7_recoil/src/recoil/todo/types.ts
+++ 7_recoil/src/recoil/todo/types.ts
@@ -1,6 +1,16 @@
 export type Status = 'active' | 'completed'
 export type Priority = 'high' | 'middle' | 'low' | 'normal'
 
+export type FilterType =
+  | 'action/complete'
+  | 'action/clear'
+  | 'status/all'
+  | 'status/active'
+  | 'status/completed'
+  | 'priority/high'
+  | 'priority/middle'
+  | 'priority/low'
+
 export interface Todo {
   id: number
   contents: string
