6df0c9dd3178769a3d58b695745594fd0d9e8f7b
jjanmo <jjanmo07@hanmail.net>
2022-12-02 04:40:44 +0900

    feat: add configuration of babel using craco

---
+++ 7_recoil/craco.config.js
@@ -0,0 +1,17 @@
+const CracoAlias = require('craco-alias')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+
+  babel: {
+    plugins: ['babel-plugin-styled-components'],
+  },
+}
