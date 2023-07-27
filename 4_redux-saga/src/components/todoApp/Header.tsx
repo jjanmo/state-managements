2524479fdb6c19c6363364b1246d29a7e18477bb
jjanmo <jjanmo07@hanmail.net>
2023-07-27 21:00:16 +0900

    update header style

--- 4_redux-saga/src/components/todoApp/Header.tsx
+++ 4_redux-saga/src/components/todoApp/Header.tsx
@@ -1,3 +1,33 @@
+import dayjs from 'dayjs'
+
 export default function Header() {
-  return <div></div>
+  const [date, dayOfWeek] = getToday()
+
+  return (
+    <div className="border-b-[1px] border-gray-200 px-10 pb-6 pt-12">
+      <p>
+        <h1 className="text-4xl font-bold">{date}</h1>
+        <h3 className="pt-3 text-xl  text-gray-400">{dayOfWeek}</h3>
+      </p>
+      <div className="mt-4 text-xl text-purple-400">
+        할 일 <span className="font-bold">{2}개</span> 남음
+      </div>
+    </div>
+  )
+}
+
+function getToday() {
+  const now = dayjs()
+
+  const dayOfWeekMap: { [key: number]: string } = {
+    0: '월',
+    1: '화',
+    2: '수',
+    3: '목',
+    4: '금',
+    5: '토',
+    6: '일',
+  }
+
+  return [`${now.year()}년 ${now.month() + 1}월 ${now.date()}일`, `${dayOfWeekMap[now.day()]}요일`]
 }
