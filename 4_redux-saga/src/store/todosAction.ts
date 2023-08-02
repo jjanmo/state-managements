105d33553f8cc41fd1be16a719ae9a9713968d85
jjanmo <jjanmo07@hanmail.net>
2023-08-02 23:57:43 +0900

    feat: add action creator

--- 4_redux-saga/src/store/todosAction.ts
+++ 4_redux-saga/src/store/todosAction.ts
@@ -1,5 +1,5 @@
-// create action
-// C
-// R
-// U
-// D
+import { createAction } from '@reduxjs/toolkit'
+
+export const addTodo = createAction('ADD_TODO')
+export const updateStatus = createAction('UPDATE_STATUS')
+export const deleteTodo = createAction('DELETE_TODO')
